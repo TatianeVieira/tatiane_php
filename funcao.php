2c3cb560391c42b9dd0edafc405b5936356ba9f7
TatianeVieira <tatiane.vieira.bruder@gmail.com>
2025-07-04 16:51:05 -0300

    Update funcao.php

--- funcao.php
+++ funcao.php
@@ -1,10 +1,18 @@
 <?php
 # index 0123456789012345
-echo $name = "stefanie hatcher";
+echo "abaixo a variavel imprime<br>";
+echo $name = "stefanie hatcher<br>";
+echo "abaixo a variavel imprime<br>";
 echo $length = strlen ($name);
+echo "<br>abaixo a variavel imprime<br>";
 echo $cmp = strcmp($name, "brian le");
+echo "<br>abaixo a variavel imprime<br>";
 echo $index = strpos ($name, "e");
-echo $name = strtoupper ($name);
+echo "<br>abaixo a variavel imprime<br>";
 echo $first = substr ($name, 9, 5);
+echo "<br>abaixo a variavel imprime<br>";
+
+echo $name = strtoupper ($name);
+
 
 ?>
